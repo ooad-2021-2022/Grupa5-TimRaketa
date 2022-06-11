1ab01882f7bfc21c0c5ed5dfb314bea400e2ea2c
selma <selmakurtovic@live.com>
2022-06-11 18:27:26 +0200

    Gotova prva migracija

--- SwapBooksApp/Data/Migrations/20220611160524_PrvaMigracija.Designer.cs
+++ SwapBooksApp/Data/Migrations/20220611160524_PrvaMigracija.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SwapBooksApp.Data;
 
 namespace SwapBooksApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220611160524_PrvaMigracija")]
+    partial class PrvaMigracija
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SwapBooksApp/Data/Migrations/20220611160524_PrvaMigracija.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SwapBooksApp.Data.Migrations
+{
+    public partial class PrvaMigracija : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notifikacija",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Poruka = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Datum = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    KorisnikId = table.Column<int>(type: "int", nullable: false),
+                    Korisnik = table.Column<int>(type: "int", nullable: false),
+                    Svrha = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notifikacija", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Racun",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Stanje = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Racun", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Korisnik",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Prezime = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Ime = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    KorisnickoIme = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Lozinka = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RacunId = table.Column<int>(type: "int", nullable: false),
+                    Vrsta = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Korisnik", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Korisnik_Racun_RacunId",
+                        column: x => x.RacunId,
+                        principalTable: "Racun",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AutorskoDjelo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naziv = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Ocjena = table.Column<double>(type: "float", nullable: false),
+                    AutorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AutorskoDjelo", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AutorskoDjelo_Korisnik_AutorId",
+                        column: x => x.AutorId,
+                        principalTable: "Korisnik",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Knjiga",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naziv = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    KorisnikId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Knjiga", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Knjiga_Korisnik_KorisnikId",
+                        column: x => x.KorisnikId,
+                        principalTable: "Korisnik",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Recenzija",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ocjena = table.Column<double>(type: "float", nullable: false),
+                    Datum = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AutorskoDjeloId = table.Column<int>(type: "int", nullable: false),
+                    KorisnikId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recenzija", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Recenzija_AutorskoDjelo_AutorskoDjeloId",
+                        column: x => x.AutorskoDjeloId,
+                        principalTable: "AutorskoDjelo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Recenzija_Korisnik_KorisnikId",
+                        column: x => x.KorisnikId,
+                        principalTable: "Korisnik",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Razmjena",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Korisnik1Id = table.Column<int>(type: "int", nullable: false),
+                    Korisnik2Id = table.Column<int>(type: "int", nullable: false),
+                    KnjigaId = table.Column<int>(type: "int", nullable: false),
+                    Datum = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Razmjena", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Razmjena_Knjiga_KnjigaId",
+                        column: x => x.KnjigaId,
+                        principalTable: "Knjiga",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Razmjena_Korisnik_Korisnik1Id",
+                        column: x => x.Korisnik1Id,
+                        principalTable: "Korisnik",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_Razmjena_Korisnik_Korisnik2Id",
+                        column: x => x.Korisnik2Id,
+                        principalTable: "Korisnik",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AutorskoDjelo_AutorId",
+                table: "AutorskoDjelo",
+                column: "AutorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Knjiga_KorisnikId",
+                table: "Knjiga",
+                column: "KorisnikId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Korisnik_RacunId",
+                table: "Korisnik",
+                column: "RacunId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Razmjena_KnjigaId",
+                table: "Razmjena",
+                column: "KnjigaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Razmjena_Korisnik1Id",
+                table: "Razmjena",
+                column: "Korisnik1Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Razmjena_Korisnik2Id",
+                table: "Razmjena",
+                column: "Korisnik2Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recenzija_AutorskoDjeloId",
+                table: "Recenzija",
+                column: "AutorskoDjeloId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recenzija_KorisnikId",
+                table: "Recenzija",
+                column: "KorisnikId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notifikacija");
+
+            migrationBuilder.DropTable(
+                name: "Razmjena");
+
+            migrationBuilder.DropTable(
+                name: "Recenzija");
+
+            migrationBuilder.DropTable(
+                name: "Knjiga");
+
+            migrationBuilder.DropTable(
+                name: "AutorskoDjelo");
+
+            migrationBuilder.DropTable(
+                name: "Korisnik");
+
+            migrationBuilder.DropTable(
+                name: "Racun");
+        }
+    }
+}
